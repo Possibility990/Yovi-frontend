02f15f3369fe93352e0d640e09ac88848db7da4e
Possibility <possibility990@gmail.com>
2026-06-24 23:02:44 +0100

    webapck setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,74 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'production',
+
     entry: './src/app.js',
-    output:{
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
     },
-    devServer:{
-        static:{
+
+    devServer: {
+        static: {
             directory: path.resolve(__dirname, 'dist')
         },
         port: 3000,
         open: true,
-        hot:true,
-        compress:true,
-        historyApiFallback:true
+        hot: true,
+        compress: true,
+        historyApiFallback: true
     },
-    module:{
-        rules:[
+
+    module: {
+        rules: [
             {
                 test: /\.css$/,
-                use:[MiniCssExtractPlugin.loader, 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use:{
-                    loader: 'babel-loader', 
-                    options:{
-                        presets:['@babel/preset-env']         
-                        
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource'
             }
         ]
     },
-    plugins:[
+
+    plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack App',
             filename: 'index.html',
             template: './src/index.html',
         }),
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: 'Account Type',
             filename: 'account-type.html',
             template: './src/account-type.html',
         }),
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: 'Signup',
             filename: 'signup.html',
             template: './src/signup.html',
         }),
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: 'Verify',
             filename: 'verify.html',
             template: './src/verify.html',
         }),
-        new MiniCssExtractPlugin()
+
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
     ]
 };
